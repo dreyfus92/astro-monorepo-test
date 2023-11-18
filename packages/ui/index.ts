2b8ef5107699844d4856b690779862aea988c953
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-11-18 17:14:08 -0600

    init

---
+++ packages/ui/index.ts
@@ -0,0 +1,5 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+
+export { default as MyComponent } from "./src/MyComponent.astro";
+export { default as MySecondComponent } from "./src/MySecondComponent.astro";
